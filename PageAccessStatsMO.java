94335a9c6e207617be66a84dbfeba989898aa2c9
nawillemsen <nawillemsen@wpi.edu>
2025-02-09 13:48:27 -0500

    Update PageAccessStatsMO.java

--- PageAccessStatsMO.java
+++ PageAccessStatsMO.java
@@ -1,5 +1,5 @@
-import java.io.IOException;
 
+import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
@@ -9,24 +9,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class PageAccessStatsMO {
-
-    public static class AccessMapper extends Mapper<Object, Text, Text, Text> {
-        private Text output = new Text();
-
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // Splitting CSV line safely
-            String[] tokens = value.toString().split(",");
-
-            // Ensure we have the correct number of columns
-            if (tokens.length >= 3) {
-                String userID = tokens[1].trim();  // ByWho
-                String pageID = tokens[2].trim();  // WhatPage
-
-                // Emit a combined output string with user stats (total accesses, unique pages)
-                output.set(userID + "\t" + pageID);
-                context.write(null, output);
-            }
-        }
+    public PageAccessStatsMO() {
     }
 
     public static void main(String[] args) throws Exception {
@@ -34,9 +17,27 @@ public class PageAccessStatsMO {
         Job job = Job.getInstance(conf, "page access stats (map-only)");
         job.setJarByClass(PageAccessStatsMO.class);
         job.setMapperClass(AccessMapper.class);
-        job.setNumReduceTasks(0);  // No reducer - map-only job
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setNumReduceTasks(0);
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class AccessMapper extends Mapper<Object, Text, Text, Text> {
+        private Text output = new Text();
+
+        public AccessMapper() {
+        }
+
+        public void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+            String[] tokens = value.toString().split(",");
+            if (tokens.length >= 3) {
+                String userID = tokens[1].trim();
+                String pageID = tokens[2].trim();
+                this.output.set(userID + "\t" + pageID);
+                context.write((Object)null, this.output);
+            }
+
+        }
     }
 }
